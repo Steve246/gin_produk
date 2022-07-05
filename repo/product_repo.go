0f0e46c52f4b34625a235082ee0299f878c835a1
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-05 10:50:39 +0700

    add retrieve

--- repo/product_repo.go
+++ repo/product_repo.go
@@ -4,10 +4,15 @@ import "gin_produk/model"
 
 type ProductRepository interface {
 	Add(newProduct *model.Product) error
+	RetrieveData() []model.Product
 }
 
 type productRepository struct {
 	productDb []model.Product
+}
+
+func (c *productRepository) RetrieveData() []model.Product {
+	return c.productDb
 }
 
 func (p *productRepository) Add(newProduct *model.Product) error {
