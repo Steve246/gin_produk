873096396f2039c0c861f0bd9fbfd2a12631d1a4
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-05 09:23:01 +0700

    first commit

--- model/product_model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Product struct {
-	ProductId   string   `json:"id" binding:"required"`
-	ProductName string   `json:"name" binding:"required"`
-	Category    Category `json:"category" binding:"required"`
-	IsActive    bool     `json:"is_active"`
-}
